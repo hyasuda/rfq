46c58cde66ef967ba926d144d35fb333cdd8e7d8
hyasuda <hyasuda.phys@gmail.com>
2018-07-26 15:06:34 +0900

    modify the vector unit before transfer matrix

--- README.md
+++ README.md
@@ -12,6 +12,7 @@ Base data is rfqll.dsta or rfqll.dat. These are the output data from RFQ simulat
 
 ### transfer.cc (string outputfile = "rfqll_trans.dat")
     - Times transfer matrix R[i][j]
+    - before trans :: x[mm]	x'[mrad]        y[mm]   y'[mrad]         z[mm]          Delta_P/P[mrad]
 
 ### rfqll.cc (string fname)
     - Derive the average of all components from RFQ output raw data "rfqll.dat"
@@ -36,6 +37,14 @@ Base data is rfqll.dsta or rfqll.dat. These are the output data from RFQ simulat
     - exclude kinetic energy < 0 data  :  Entry is 16,116 -> 16,114  (2 data is died)
     - Transfer matrix is made by TRACE3D "~/data/muonLinac/TRACE3D/RFQoutput_4QM_gap_matching.t3d"
         x[mm]	x'[mrad]        y[mm]   y'[mrad]         phase[deg]	KineticEnergy[MeV]
+
+### rfqll_trans_01.dat
+    - change the unit as below
+    - origin       :: x[mm]	x'[mrad]        y[mm]   y'[mrad]         phase[deg]	KineticEnergy[MeV]
+    - before trans :: x[mm]	x'[mrad]        y[mm]   y'[mrad]         z[mm]          Delta_P/P[mrad]
+
+### rfqll_trans_test.dat
+    - For test of "~/macro/muonLinac/rfq/transfer.cc"
 
 ### rfqll_trans_output.dat
     - Convert the transfered file (rfqll_trans.dat) to TurtleFile format.
--- transfer.cc
+++ transfer.cc
@@ -8,10 +8,12 @@
 using namespace std;
 
 
-const double m_mu  = 105.66E+6; // muon mass [eV]
-// const double m_e   = 511E+3   ; // electron mass [eV]
-const double c     = 299792458; // light speed [m/s]
-const double pi    = 3.1415   ; // PI
+const double m_mu   = 105.66    ; // muon mass [MeV]
+const double m_e    = 511E+3    ; // electron mass [eV]
+const double c      = 299792458 ; // light speed [m/s]
+const double pi     = 3.1415    ; // PI
+const double RF     = 324E+6    ; // RF frequency [Hz]
+const double lambda = 925.285   ; // RF wave-length [mm] : c/RF
 
 const string inputfile = "rfqll.dat";
 
@@ -25,12 +27,6 @@ const int n = 6;
 // double Ek;
 
 // Average //
-double xmean = 0;
-double xpmean = 0;
-double ymean = 0;
-double ypmean = 0;
-double phimean = 0;
-double Wmean = 0;
 int nline = 0;
 
 // Transfer components //
@@ -38,6 +34,10 @@ double X[n]     = {}; // x, xp, y, yp, phi, Ek
 double Xmean[n] = {}; // x, xp, y, yp, phi, Ek
 double Y[n]     = {}; // x, xp, y, yp, phi, Ek
 // double Y[n];
+
+// relativistic parameter //
+double beta  = 0;
+double Gamma = 0;
 
 // Transfer matrix definition //
 // const int n = 6;
@@ -50,19 +50,20 @@ const double R[n][n] = {
     {  0.   ,  0.   ,  0.   ,  0.   , -3.529, -0.579},
 };
 
-
 // function definition //
 void mean(string fname);
-void diff();
+void beta_calc( double ke);
+void Gamma_calc( double beta_val );
 void unit();
 void trans();
 void unitorigin();
-void add();
 void reset();
 
 void transfer(string outputfile = "rfqll_trans.dat"){
 
     mean( inputfile );
+    beta_calc( Xmean[5] );
+    Gamma_calc( beta );
 
     stringstream filename;
     stringstream output;
@@ -72,13 +73,19 @@ void transfer(string outputfile = "rfqll_trans.dat"){
     int nline = 0;
 
     while(ifs >> X[0] >> X[1] >> X[2] >> X[3] >> X[4] >> X[5] ){
-        // cout << "nline : " << nline << endl;
-        diff();
+        cout << "nline : " << nline << endl;
+        // if(nline > 3) break; // for debug
+        for(int i = 0 ; i < 6 ; i++ ){
+            cout << X[i] << "\t" ;
+        }
+        cout << endl;
         unit();
         trans();
         unitorigin();
-        add();
         if(Y[5] < 0 ) continue;
+        for(int i = 0 ; i < 6 ; i++ ){
+            cout << Y[i] << "\t" ;
+        }
         ofs  << scientific;
         ofs  << Y[0] << "\t" << Y[1] << "\t" << Y[2] << "\t" << Y[3] << "\t" << Y[4] << "\t" << Y[5] << endl;
         nline++;
@@ -95,12 +102,6 @@ void mean(string fname){
     double ypsum = 0;
     double phisum = 0;
     double Wsum = 0;
-    double xmean = 0;
-    double xpmean = 0;
-    double ymean = 0;
-    double ypmean = 0;
-    double phimean = 0;
-    double Wmean = 0;
 
     ifstream rf(Form("/Users/YASUDA/data/muonLinac/rfq/%s", fname.c_str()));
 
@@ -113,32 +114,47 @@ void mean(string fname){
         Wsum   += W;
         nline++;
     }
-    xmean   = xsum/nline;
-    xpmean  = xpsum/nline;
-    ymean   = ysum/nline;
-    ypmean  = ypsum/nline;
-    phimean = phisum/nline;
-    Wmean   = Wsum/nline;
-    Xmean[0] = xmean;
-    Xmean[1] = xpmean;
-    Xmean[2] = ymean;
-    Xmean[3] = ypmean;
-    Xmean[4] = phimean;
-    Xmean[5] = Wmean;
+    Xmean[0] = xsum/nline;
+    Xmean[1] = xpsum/nline;
+    Xmean[2] = ysum/nline;
+    Xmean[3] = ypsum/nline;
+    Xmean[4] = phisum/nline;
+    Xmean[5] = Wsum/nline;
     // cout << "#####  AVERAGE  #####" << endl;
+    // cout << scientific ; cout.precision(4);
     // cout << "Enrty = " << nline << endl;
-    // cout << "x" << "\t" << "xp" << "\t" << "y" << "\t" << "yp" << "\t" << "phi" << "\t" << "W" << endl;
-    // cout << xmean << "\t" << xpmean << "\t" << ymean << "\t" << ypmean << "\t" << phimean << "\t" << Wmean << endl;
+    // cout << "x[mm]   " << "\t" << "xp[mrad]" << "\t" << "y[mm]   " << "\t" << "yp[mrad]" << "\t" << "phi[deg]" << "\t" << "W[MeV]" << endl;
+    // cout << Xmean[0] << "\t" << Xmean[1] << "\t" << Xmean[2] << "\t" << Xmean[3] << "\t" << Xmean[4] << "\t" << Xmean[5] << endl;
 }
 
-void diff(){
-    for(int i = 0 ; i < n ; i++){
-        X[i] = X[i] - Xmean[i];
-    }
+void beta_calc(double ke){
+    double mom = sqrt(2 * m_mu * ke);
+    beta = mom / sqrt(m_mu*m_mu + mom*mom);
+    cout << scientific;
+    cout << "beta = " << beta << " ,\t";
 }
+
+void Gamma_calc( double beta_val ){
+    Gamma = 1/sqrt( 1 - beta_val * beta_val );
+    cout << "Gamma = " << Gamma << endl;
+}
+
+// void diff(){
+//     cout << "after diff :: " ;
+//     for(int i = 0 ; i < n ; i++){
+//         X[i] = X[i] - Xmean[i];
+//         if(i == 5) cout << X[i] << "\t" ;
+//     }
+//     cout << endl;
+// }
 
 void unit(){
-    X[5] = X[5] * 1.e+3; // MeV -> keV
+    Y[0] = X[0] - Xmean[0];
+    Y[1] = X[1] - Xmean[1];
+    Y[2] = X[2] - Xmean[2];
+    Y[3] = X[3] - Xmean[3];
+    Y[4] = -beta * lambda / 360 * ( X[4] - Xmean[4] );
+    Y[5] = Gamma/(Gamma + 1) * (X[5] - Xmean[5])/X[5] * 1.e+3;
 }
 
 void trans(){
@@ -149,15 +165,23 @@ void trans(){
     }
 }
 
-void add(){
-    for(int i = 0 ; i < n ; i++){
-        Y[i] = Y[i] + Xmean[i];
-    }
+void unitorigin(){
+    Y[0] = Y[0] + Xmean[0]; // mm
+    Y[1] = Y[1] + Xmean[1]; // mm
+    Y[2] = Y[2] + Xmean[2]; // mm
+    Y[3] = Y[3] + Xmean[3]; // mm
+    Y[4] = -360/(beta * lambda) * Y[4]; // mm
+    Y[5] = Y[5] * 1.e-3; // mrad -> rad
+    Y[5] = Gamma * Xmean[5] / (Gamma - (Gamma + 1) * Y[5]); // [MeV]
+    // Y[5] = Xmean[5] * (1 + (Gamma + 1)/Gamma * Y[5]); // delta W / W ~ (W-<W>)/<W> [MeV]
 }
 
-void unitorigin(){
-    Y[5] = Y[5] * 1.e-3; // keV -> MeV
-}
+// void add(){
+//     for(int i = 0 ; i < n ; i++){
+//         Y[i] = Y[i] + Xmean[i];
+//         if(i == 5)cout << "after add :: " << Y[i ] << endl;
+//     }
+// }
 
 void reset(){
     for(int i = 0 ; i < n ; i++){
